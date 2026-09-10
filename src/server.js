482969cfc51444b8ddab1a26acc32284b4d1224e
Sunny Vishwakarma <sunnyvishwakarma933@gmail.com>
2026-09-10 19:32:30 +0530

    Connect backend to MongoDB Atlas

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 const PORT = 5000;
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("CivicGuardian Backend is Running!");
